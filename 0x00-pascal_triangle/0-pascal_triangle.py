778bafb592171465fd8648c895c75545afdb081c
kidus-t-w <kidusk69@gmail.com>
2024-04-11 22:35:36 +0300

    Fixed task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,16 +6,18 @@ Pascal's Triangle
 
 def pascal_triangle(n):
     """Create a function called `pascal_triangle(n)` that
-      returns a list containing n lists of integers, 
+      returns a list containing n lists of integers,
       representing the rows of Pascal's triangle up to the nth row.
     """
-    res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            res.append(level)
-    return res
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
