dac66d64c678231a88dea4a09833d6d11607074f
kidus-t-w <kidusk69@gmail.com>
2024-05-16 23:31:43 +0300

    Modified Task

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -19,53 +19,24 @@ n = int(sys.argv[1])
 
 
 def queens(n, i=0, a=[], b=[], c=[]):
-    """
-    Find possible positions for N queens on an nxn chessboard.
-
-    Args:
-        n (int): The size of the chessboard.
-        i (int, optional): The current row being filled. Defaults to 0.
-        a (list, optional): The current column positions of the queens.
-        b (list, optional): The current diagonals formed by the queen.
-        c (list, optional): The current diagonals formed by the queen.
-    Yields:
-        list: A list of column positions for the queens in the current row.
-    """
-    # Base case: all queens have been placed
+    """ find possible positions """
     if i < n:
         for j in range(n):
-            # Check if the queen can be placed in the current column
             if j not in a and i + j not in b and i - j not in c:
-                # Recursively find possible positions for the next row
                 yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
-        # Return the current row of queen positions
         yield a
 
 
 def solve(n):
-    """
-    Solve the N-queens problem by iterating through all possible solutions
-    and printing each one.
-
-    Args:
-        n (int): The size of the chessboard.
-    """
-    # Initialize an empty list to hold the current solution
+    """ solve """
     k = []
-    # Initialize an index to keep track of the current column
     i = 0
-    # Iterate through all possible solutions
     for solution in queens(n, 0):
-        # Iterate through the columns in the current solution
         for s in solution:
-            # Add the current column and index to the current solution
             k.append([i, s])
-            # Increment the index
             i += 1
-        # Print the current solution
         print(k)
-        # Reset the current solution and index
         k = []
         i = 0
 
